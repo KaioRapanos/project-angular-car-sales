7384e7319d7cb0ffe1ad160c171e758eb9e084d9
KaioRapanos <kaiorapanos@gmail.com>
2024-06-11 08:25:01 -0300

    canActivate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
+import { AuthGuardService } from './resources/service/auth-guard.service';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'dashboard',
     loadChildren: () => import('./views/dashboard/dashboard.module')
-                .then(m => m.DashboardModule)
-  }
+                .then(m => m.DashboardModule), canActivate: [AuthGuardService]
+  },
+  {path: '**', redirectTo: 'login'}
 ];
